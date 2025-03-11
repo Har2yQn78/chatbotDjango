63eecc1781a4dc231357017e16c2b4e93f441bf4
Har2yQn78 <hamidreza.amiri800@gmail.com>
2025-03-11 17:15:47 +0330

    setup NEON DATABASE

--- src/hrhome/settings.py
+++ src/hrhome/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from decouple import config
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x*83jan0y*o0)2+1nsgem$qptbw&u8-v2r++nq36fg(vekprw@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DJANGO_DEBUG", cast=bool, default=False)
 
 ALLOWED_HOSTS = []
 
@@ -81,8 +83,15 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASE_URL = config('DATABASE_URL', default="", cast=str)
 
-
+if DATABASE_URL != "":
+    DATABASES = {
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+         conn_max_age=0
+    )
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
