40f21864f9bafefa5d446c1e89d79e5060c77599
Har2yQn78 <hamidreza.amiri800@gmail.com>
2025-03-13 12:38:58 +0330

    update service to generate the embedding

--- src/data/models.py
+++ src/data/models.py
@@ -17,5 +17,15 @@ class BlogPost(models.Model):
     embedding = VectorField(dimensions=EMEDDING_LENGTH, blank=True, null=True)
     can_delete = models.BooleanField(default=False, help_text="Use in jupyter notebooks")
 
+    def save(self, *args, **kwargs):
+        has_changed = False
+        if self._content != self.content:
+            has_changed = True
+        if (self.embedding is None) or has_changed == True:
+            raw_embedding_text = self.get_embedding_text_raw()
+            if raw_embedding_text is not None:
+                self.embedding = services.get_embedding(raw_embedding_text)
+        super().save(*args, **kwargs)
+
     def get_embedding_text_raw(self):
         return self.content
